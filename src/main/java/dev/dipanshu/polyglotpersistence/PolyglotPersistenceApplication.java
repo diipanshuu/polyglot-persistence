6e03b1123f7313cc64a9471302a8c2918c397646
diipanshuu <diipanshuu@gmail.com>
2024-06-05 16:11:02 +0530

    Bootstrapp application using spring initializer

---
+++ src/main/java/dev/dipanshu/polyglotpersistence/PolyglotPersistenceApplication.java
@@ -0,0 +1,13 @@
+package dev.dipanshu.polyglotpersistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyglotPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyglotPersistenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dipanshu/polyglotpersistence/PolyglotPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dipanshu.polyglotpersistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyglotPersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
